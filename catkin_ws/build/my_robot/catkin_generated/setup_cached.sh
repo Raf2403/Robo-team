ce143a115eb55ae9b48ca2445b54a0c47549d186
egorryb <egorarybkin@mail.ru>
2025-05-01 01:16:19 +0300

    First Catkin space and new DockerFile

---
+++ catkin_ws/build/my_robot/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/catkin_ws/devel/.private/my_robot:$CMAKE_PREFIX_PATH"
+export PWD='/catkin_ws/build/my_robot'
+export ROSLISP_PACKAGE_DIRECTORIES="/catkin_ws/devel/.private/my_robot/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/catkin_ws/src/my_robot:$ROS_PACKAGE_PATH"
